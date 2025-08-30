8d8a2a5a7620d30251af2af1d2744066a4461da2
mohsen tabar <mohsnmasoumi73@gmail.com>
2025-08-30 14:29:05 +0330

    tet3

--- hello.py
+++ hello.py
@@ -1,2 +1,11 @@
-name = input('name: ')
-print ("hello "+name)
+name = "mohsen"
+print (name)
+age = int (input("input your age: "))
+print(f"hello {name}")
+
+if age > 20:
+    print("a")
+elif age == 20 :
+    print("b") 
+else :
+    print("c")       
---
+++ type.py
@@ -0,0 +1,11 @@
+name = "mohsen"
+print(name[0])
+
+names = ["mohsen","aysan","Nastaran"]
+print (names[0])
+
+names.append("sina")
+print(names)
+
+names.sort()
+print(names)
