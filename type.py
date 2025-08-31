5edfd621ad351bf50e01e78a892d1c7242754a95
mohsen tabar <mohsnmasoumi73@gmail.com>
2025-08-31 12:40:16 +0330

    day2

--- type.py
+++ type.py
@@ -1,3 +1,4 @@
+#list
 name = "mohsen"
 print(name[0])
 
@@ -6,6 +7,25 @@ print (names[0])
 
 names.append("sina")
 print(names)
-
 names.sort()
 print(names)
+
+#tuple
+point = (12.5 , 25)
+print (point[0])
+
+#set
+s = set()
+s.add(1)
+s.add(2)
+s.add(3)
+s.add(2)
+print(s)
+
+#dict
+person = {
+    "name" : "aysan",
+    "age" : 22 ,
+}
+print(person)
+print(person["age"])
