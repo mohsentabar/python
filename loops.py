6408ff6cef8cdd34cc6828c2820f8e42f5ed136c
mohsen tabar <mohsnmasoumi73@gmail.com>
2025-08-31 13:02:01 +0330

    day2.2

---
+++ loops.py
@@ -0,0 +1,17 @@
+for i in [1,2,3,4,5,6] :
+    print(i)
+
+for i in range(20):
+    print(i)    
+
+for j in "aysan":
+    print(j)    
+
+names = ["mohsen","reaz","aysan"]
+for name in names:
+    print(name)    
+
+num = [1,2,3,4,5,6] 
+for n in num :
+    print(n*n)
+
